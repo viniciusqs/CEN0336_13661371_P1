3bf695141b3cc7ceed1fad589b294aa7b76ce793
viniciusqs <viniciusqueiroz@usp.br>
2024-10-14 19:28:06 -0300

    Create bash_script.sh

---
+++ bash_script.sh
@@ -0,0 +1,9 @@
+
+# Comando que mostra o diretório onde você está
+pwd
+
+# Comando que gera uma lista com detalhes do conteúdo da pasta HOME e redireciona para lista_HOME.txt
+ls -l ~ > lista_HOME.txt
+
+# Comando que imprime a data atual na tela
+date
